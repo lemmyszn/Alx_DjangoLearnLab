b5417ce53bf30d4ee57e8ff90ac54e6105b95f69
lemmyszsn <lemmyxking@gmail.com>
2024-08-11 11:46:08 +0300

    add django project

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/models.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/models.py
@@ -5,7 +5,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     published_date = models.DateField()
-    publication_year = models.PositiveIntegerField()  # Add this line
+    publication_year = models.PositiveIntegerField(default=2000)  # Set default value
     isbn = models.CharField(max_length=13, unique=True)
     pages = models.PositiveIntegerField()
     cover = models.URLField(blank=True, null=True)

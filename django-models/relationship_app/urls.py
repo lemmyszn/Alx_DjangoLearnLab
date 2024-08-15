f20375ef10fc48181ded0b192128edf83503dbd0
lemmyszsn <lemmyxking@gmail.com>
2024-08-15 12:10:27 +0300

    add django app

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -5,3 +5,14 @@ urlpatterns = [
     path('books/', list_books, name='list_books'),  # Function-based view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
 ]
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import register, list_books, LibraryDetailView
+
+urlpatterns = [
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', register, name='register'),
+]

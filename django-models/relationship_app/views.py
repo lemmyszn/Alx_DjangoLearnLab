fb62ea85c3cc72a19aec564829c7f1b75cd41239
lemmyszsn <lemmyxking@gmail.com>
2024-08-15 11:47:26 +0300

    add django project

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -6,8 +6,7 @@ from .models import Book
 
 def list_books(request):
     books = Book.objects.all()
-    output = "\n".join([f"{book.title} by {book.author.name}" for book in books])
-    return HttpResponse(output, content_type="text/plain")
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 from django.views.generic import DetailView
 from .models import Library

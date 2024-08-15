968e81f28b2c908729f19df2df66d6f3fe61fe26
lemmyszsn <lemmyxking@gmail.com>
2024-08-15 12:44:50 +0300

    add django app

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -32,3 +32,31 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+
+# Helper functions to check roles
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+# Admin view
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+# Librarian view
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+# Member view
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')

aa26bbd76a8857d02a37e75c99668f96c5a6a2eb
lemmyszsn <lemmyxking@gmail.com>
2024-08-23 09:22:15 +0300

    add django app

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -32,3 +32,14 @@ class BookAdmin(admin.ModelAdmin):
         models.CharField: {'widget': admin.widgets.AdminTextInputWidget}, 
     }
 
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)

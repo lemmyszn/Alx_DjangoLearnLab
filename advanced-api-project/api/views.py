243a3311ff99e6eed950da77637912273790e86a
lemmyszsn <lemmyxking@gmail.com>
2024-09-07 13:54:30 +0300

    add django project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -15,3 +15,26 @@ class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+from rest_framework import generics
+from .models import Book
+from .seriealizers import BookSerializer
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+
+# CreateView for adding a new book
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+# UpdateView for updating an existing book
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+# DeleteView for deleting an existing book
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]

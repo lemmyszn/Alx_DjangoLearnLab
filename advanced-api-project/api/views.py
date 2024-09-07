820b9fe29c3677a2e733bf9e0f9c5b6ca635d0b6
lemmyszsn <lemmyxking@gmail.com>
2024-09-07 14:02:26 +0300

    add django project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,40 +1,54 @@
-from django.shortcuts import render
+# api/views.py
 
-# Create your views here.
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .seriealizers import BookSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-class BookListView(generics.ListCreateAPIView):
+# ListView and CreateView combined
+class BookListCreateView(generics.ListCreateAPIView):
+    """
+    get:
+    Return a list of all existing books.
+
+    post:
+    Create a new book instance. Requires authentication.
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    def perform_create(self, serializer):
+        """
+        Optionally customize the creation of a Book instance.
+        For example, you can associate the book with the logged-in user.
+        """
+        serializer.save()
+
+# DetailView, UpdateView, and DeleteView combined
+class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    get:
+    Retrieve a single book by its ID.
+
+    put:
+    Update an existing book. Requires authentication.
+
+    delete:
+    Delete a book. Requires authentication.
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-from rest_framework import generics
-from .models import Book
-from .seriealizers import BookSerializer
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+    def perform_update(self, serializer):
+        """
+        Optionally customize the update of a Book instance.
+        """
+        serializer.save()
 
-# CreateView for adding a new book
-class BookCreateView(generics.CreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]
-
-# UpdateView for updating an existing book
-class BookUpdateView(generics.UpdateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]
-
-# DeleteView for deleting an existing book
-class BookDeleteView(generics.DestroyAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]
+    def perform_destroy(self, instance):
+        """
+        Optionally customize the deletion of a Book instance.
+        """
+        instance.delete()

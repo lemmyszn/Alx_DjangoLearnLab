b5b755d776da99f533587d9393252ffdfbd1704c
lemmyszsn <lemmyxking@gmail.com>
2024-09-07 14:39:08 +0300

    add django project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -52,3 +52,16 @@ class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
         Optionally customize the deletion of a Book instance.
         """
         instance.delete()
+from rest_framework import generics
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend  # Import DjangoFilterBackend
+from .models import Book
+from .seriealizers import BookSerializer
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['title', 'author', 'publication_year']
+    search_fields = ['title', 'author']
+    ordering_fields = ['title', 'publication_year']

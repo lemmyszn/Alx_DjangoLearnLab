f83b61180f14d28fb7602ae6477a660374e6366d
lemmyszsn <lemmyxking@gmail.com>
2024-09-07 15:26:33 +0300

    add django project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.filters import SearchFilter, OrderingFilter  # Import Orderi
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .seriealizers import BookSerializer
+from django_filters import rest_framework
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()

f8562753af6d07b792c5211af0a0da52c1c53f40
lemmyszsn <lemmyxking@gmail.com>
2024-09-08 21:22:17 +0300

    add django project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,15 +1,28 @@
 from rest_framework import generics
-from rest_framework.filters import SearchFilter, OrderingFilter  # Import OrderingFilter
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as filters
 from .models import Book
-from .seriealizers import BookSerializer
-from django_filters import rest_framework
+from seriealizers import BookSerializer
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]  # Add OrderingFilter here
-    filterset_fields = ['title', 'author', 'publication_year']  # Enable filtering by these fields
-    search_fields = ['title', 'author']  # Enable search by title and author
-    ordering_fields = ['title', 'publication_year']  # Enable ordering by title and publication year
+    filter_backends = [filters.DjangoFilterBackend]
+    filterset_fields = ['title', 'author', 'publication_year']
+
+from rest_framework.filters import SearchFilter
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [filters.DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['title', 'author', 'publication_year']
+    search_fields = ['title', 'author__name']
+
+from rest_framework.filters import OrderingFilter
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [filters.DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['title', 'author', 'publication_year']
+    search_fields = ['title', 'author__name']
+    ordering_fields = ['title', 'publication_year']
 

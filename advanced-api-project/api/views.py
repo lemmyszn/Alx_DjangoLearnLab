927a3a71fe0aed3202c8229c418be7f1e7bc309b
lemmyszsn <lemmyxking@gmail.com>
2024-09-09 19:27:09 +0300

    add django project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -28,3 +28,26 @@ class BookListView(generics.ListAPIView):
     search_fields = ['title', 'author__name']
     ordering_fields = ['title', 'publication_year']
 
+from rest_framework import generics
+from .models import Book
+from .seriealizers import BookSerializer
+
+# DetailView for a single book
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+# CreateView for adding a new book
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# UpdateView for modifying an existing book
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# DeleteView for removing a book
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+

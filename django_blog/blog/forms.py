c7edc0ba6cb198fad7b7cbbfe530c9d823d195e6
lemmyszsn <lemmyxking@gmail.com>
2024-09-11 13:09:38 +0300

    add django project

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -8,3 +8,11 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+# blog/forms.py
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']

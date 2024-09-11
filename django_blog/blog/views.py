97012da67c2ee97ed4b6643455ec07dd16c68fcd
lemmyszsn <lemmyxking@gmail.com>
2024-09-11 12:10:40 +0300

    add django project

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.contrib.auth.views import LoginView, LogoutView
+
+class UserLoginView(LoginView):
+    template_name = 'blog/login.html'
+
+class UserLogoutView(LogoutView):
+    template_name = 'blog/logout.html'
+
+from django.shortcuts import render , redirect 
+from django.contrib.auth import login
+from .forms import UserRegisterForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('blog:home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+

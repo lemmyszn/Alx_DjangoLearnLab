9a7ad664997287b8d3d8dcd74b3e92848c941ce2
lemmyszsn <lemmyxking@gmail.com>
2024-09-14 22:45:00 +0300

    build a django app

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
 
     # Comment-related URLs
     path('post/<int:post_id>/comments/new/', add_comment, name='add-comment'),
-    path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='update-comment'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='update-comment'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete-comment'),
     path('post/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='add-comment'),
 

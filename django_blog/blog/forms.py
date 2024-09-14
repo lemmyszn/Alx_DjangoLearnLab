ba1923b68bb71d3cfe38cd92b5fc1981a76b6a62
lemmyszsn <lemmyxking@gmail.com>
2024-09-14 22:58:45 +0300

    build a complete django app

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from blog.models import Post, Tag, Comment
+from django.forms.widgets import CheckboxSelectMultiple
 
 # User Registration Form
 class UserRegisterForm(UserCreationForm):

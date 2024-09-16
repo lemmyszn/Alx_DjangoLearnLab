bc51a46caccc77ae632ce565653e079b4f8ed58e
lemmyszsn <lemmyxking@gmail.com>
2024-09-16 16:52:05 +0300

    build and deploy drf app

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from .models import CustomUser
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'password', 'email']
+    
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(**validated_data)
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Invalid Credentials")
+

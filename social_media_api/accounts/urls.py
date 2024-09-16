51fd1164e79e27deadd17c4910b19ed75bd19ead
lemmyszsn <lemmyxking@gmail.com>
2024-09-16 17:11:13 +0300

    build and deploy drf app

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RegisterUserView
+from .views import RegisterUserView, CustomAuthToken
 
 urlpatterns = [
       path('register/', RegisterUserView.as_view(), name='register'),
+        path('login/', CustomAuthToken.as_view(), name='login'), 
 ]

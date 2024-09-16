3d66d043c5eccd75486622ad1b3630326a12cd23
lemmyszsn <lemmyxking@gmail.com>
2024-09-16 17:04:27 +0300

    build and deploy drf app

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -3,29 +3,31 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
-# Import your CustomUser model
+# Get the user model (CustomUser in your case)
 User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    # Use CharField for password (write-only to ensure it's not included in output)
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'bio', 'profile_picture')
+        fields = ('id', 'username', 'email', 'password', 'bio', 'profile_picture')
 
     def create(self, validated_data):
-        # Create the user using the `create_user` method
+        """
+        Custom method to create a user with the hashed password and generate a token.
+        """
+        # Use create_user() to handle password hashing
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            password=validated_data['password'],
+            password=validated_data['password'],  # Password will be hashed here
             bio=validated_data.get('bio', ''),
             profile_picture=validated_data.get('profile_picture', None)
         )
 
-        # Generate a token for the newly created user
+        # Automatically create an authentication token for the new user
         Token.objects.create(user=user)
 
         return user
-
-

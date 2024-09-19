b5f55530bab904aee4ce01cbfbec6b035a39bb81
lemmyszsn <lemmyxking@gmail.com>
2024-09-19 13:04:45 +0300

    build and deploy drf app

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -6,6 +6,8 @@ from rest_framework import viewsets, permissions, filters
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsAuthorOrReadOnly
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
@@ -28,3 +30,13 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         post = Post.objects.get(pk=self.kwargs['post_pk'])
         serializer.save(author=self.request.user, post=post)
+
+class UserFeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Get the list of users the current user is following
+        following_users = self.request.user.following.all()
+        # Return posts from these users, ordered by creation date
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')

36579c10624480e7bbe13b68e6bc31f471cc6aed
lemmyszsn <lemmyxking@gmail.com>
2024-09-19 13:52:20 +0300

    build and deploy DRF app

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -48,7 +48,7 @@ class LikePostView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, pk, *args, **kwargs):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         like, created = Like.objects.get_or_create(user=request.user, post=post)
         
         if not created:
@@ -68,7 +68,7 @@ class UnlikePostView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, pk, *args, **kwargs):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         like = Like.objects.filter(user=request.user, post=post).first()
         
         if not like:
